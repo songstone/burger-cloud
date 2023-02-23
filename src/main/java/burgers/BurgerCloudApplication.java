bcd345eea297248d3d431a67d7d7be154d0927d8
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-02-24 08:09:12 +0900

    feat: init data

--- src/main/java/burgers/BurgerCloudApplication.java
+++ src/main/java/burgers/BurgerCloudApplication.java
@@ -1,7 +1,13 @@
 package burgers;
 
+import burgers.data.IngredientRepository;
+import burgers.domain.Ingredient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static burgers.domain.Ingredient.*;
 
 @SpringBootApplication
 public class BurgerCloudApplication {
@@ -10,4 +16,19 @@ public class BurgerCloudApplication {
         SpringApplication.run(BurgerCloudApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+             repo.save(new Ingredient("PLBN" ,"Plain Bun",    Type.WRAP));
+             repo.save(new Ingredient("BABN", "Bagel Bun",    Type.WRAP));
+             repo.save(new Ingredient("GRBF", "Ground Beef",  Type.PROTEIN));
+             repo.save(new Ingredient("BACO", "Bacon",        Type.PROTEIN));
+             repo.save(new Ingredient("TMTO", "Tomato",       Type.VEGGIES));
+             repo.save(new Ingredient("LETC", "Lettuce",      Type.VEGGIES));
+             repo.save(new Ingredient("CHED", "Cheddar",      Type.CHEESE));
+             repo.save(new Ingredient("MOZZ", "Mozzarella",   Type.CHEESE));
+             repo.save(new Ingredient("SLSA", "Salsa",        Type.SAUCE));
+             repo.save(new Ingredient("SRCR", "Sour Cream",   Type.SAUCE));
+        };
+    }
 }
